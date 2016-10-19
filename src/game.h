8d4afdf42fcd7dbb27e0a7e38922f07fe5e690d8
Znoraka <znoraka@gmail.com>
2016-10-19 17:37:48 +0200

    flappy fonctionnel

---
+++ src/game.h
@@ -0,0 +1,224 @@
+#pragma once
+
+class Bird {
+private:
+  float x;
+
+  float gravity = 2.0f;
+
+  float upspeed = 0;
+
+  float slowDown = 0.5f;
+
+  Network *network;
+  
+public:
+  bool alive = true;
+  float y;
+  int score = 0;
+
+  void draw();
+  Network *getNetwork();
+  void setNetwork(Network *network);
+  void jump();
+  int getScore();
+  void update();
+  Bird(float x, float y);
+
+};
+
+class Pipe {
+private:
+  float x, yTop, yBot;
+
+public:
+  void draw();
+  void update();
+  void init(float x, float yTop);
+  float getX();
+  float getYTop();
+  float getYBot();
+  Pipe(float x, float yTop);
+};
+
+class Game {
+private:
+  std::vector<Bird*> birds;
+
+  Pipe *pipe1, *pipe2;  
+public:
+  int bestScore = 0;
+  void update();
+  Game();
+
+  int aliveBirds = 50;
+  int generation = 0;
+
+  Pipe *getPipe1();
+  Pipe *getPipe2();
+  void connectNetworks(std::vector<Network*> network);
+
+};
+
+static Game *game = new Game();
+
+Bird::Bird(float x, float y) : x(x), y(y) {
+  
+}
+
+Pipe::Pipe(float x, float yTop) {
+  init(x, yTop);
+}
+
+void Pipe::init(float x, float yTop) {
+  this->x = x;
+  this->yTop = yTop;
+  this->yBot = yTop + 6;
+}
+
+void Bird::draw() {
+  if(alive)
+    mvprintw(y, x, "0>");
+}
+
+void Pipe::draw() {
+  for (int i = 0; i < yTop; i++) {
+    mvprintw(i, x, "||||||||");
+  }
+
+  for (int i = yBot; i < 50; i++) {
+    mvprintw(i, x, "||||||||");
+  }
+}
+
+int Bird::getScore() {
+  return score;
+}
+
+void Pipe::update() {
+  x--;
+}
+
+float Pipe::getX() {
+  return this->x;
+}
+
+float Pipe::getYTop() {
+  return this->yTop;
+}
+
+float Pipe::getYBot() {
+  return this->yBot;
+}
+
+
+void Bird::jump() {
+  upspeed = -1;
+}
+
+void Bird::update() {
+  if(alive) {
+    y += gravity + ((upspeed < 0) ? -3 : 0);
+
+    if(upspeed <= 0) 
+      upspeed += slowDown;
+  
+    score++;
+
+    if(game->getPipe1()->getX() <= 2 && (y > game->getPipe1()->getYBot() || y < game->getPipe1()->getYTop())) {
+      this->alive = false;
+      game->aliveBirds--;
+    }
+
+    network->compute();
+    if(network->getOutputs()[0]->getOutput() > 0.5) {
+      jump();
+    }
+  }
+}
+
+Game::Game() {
+  pipe1 = new Pipe(55, std::rand() % 10 + 5);
+  pipe2 = new Pipe(95, std::rand() % 10 + 5);
+
+  for (int i = 0; i < 50; i++) {
+    birds.push_back(new Bird(1, std::rand() % 20 + 5));
+  }
+}
+
+Pipe *Game::getPipe1() {
+  return pipe1;
+}
+
+Pipe *Game::getPipe2() {
+  return pipe2;
+}
+
+void Game::update() {
+  pipe1->update();
+  pipe2->update();
+
+  pipe1->draw();
+  pipe2->draw();
+
+  if(pipe1->getX() <= -2) {
+    Pipe *tmp = pipe1;
+    pipe1 = pipe2;
+    pipe2 = tmp;
+    pipe2->init(pipe1->getX() + 40, std::rand() % 10 + 5);
+  }
+
+  for(auto i : birds) {
+    i->update();
+    i->draw();
+    i->getNetwork()->setInputs({i->y, pipe1->getYBot()});
+  }
+
+  if(aliveBirds == 0) {
+    generation++;
+    pipe1->init(55, std::rand() % 10 + 5);
+    pipe2->init(95, std::rand() % 10 + 5);
+    aliveBirds = 50;
+    int bestScore = 0;
+    int index = 0;		  
+
+    for (int i = 0; i < birds.size(); i++) {
+      birds[i]->alive = true;
+      birds[i]->y = std::rand() % 20 + 5;
+      if(birds[i]->getScore() > bestScore) {
+	bestScore = birds[i]->getScore();
+	index = i;
+      }
+    }
+
+    if(bestScore > this->bestScore)
+      this->bestScore = bestScore;
+
+    for (int i = 0; i < birds.size(); i++) {
+      birds[i]->score = 0;
+      if(i != index) {
+	birds[i]->setNetwork(birds[i]->getNetwork());
+	birds[i]->getNetwork()->mutate(0.05f);
+      }
+    }
+  }
+
+  mvprintw(0, 0, "bestScore = %d", this->bestScore);
+  mvprintw(1, 0, "alive = %d", aliveBirds);
+  mvprintw(2, 0, "generation = %d", generation);
+}
+
+void Game::connectNetworks(std::vector<Network*> networks) {
+  for (int i = 0; i < networks.size(); i++) {
+    birds[i]->setNetwork(networks[i]);
+  }
+}
+
+void Bird::setNetwork(Network *network) {
+  this->network = network;
+}
+
+Network *Bird::getNetwork() {
+  return this->network;
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,18 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-
+#include <fstream>
+#include <ncurses.h>
+#include <chrono>
+#include <thread>
 #include "neuron.h"
 #include "network.h"
+#include "game.h"
 
 int main(int argc, char **argv) {
   std::srand(std::time(0));
-  // std::srand(1);
 
-  std::vector<int> dimensions{3, 4, 1};
+  std::vector<int> dimensions{2, 8, 1};
   std::vector<Network*> networks;
 
   for (int i = 0; i < 50; i++) {
@@ -22,46 +25,48 @@ int main(int argc, char **argv) {
     i->compute();
   }
 
-  std::vector<std::vector<float> > inputs = {{0, 0, 1}, {1, 1, 1}, {1, 0, 1}, {0, 1, 1}};
-  std::vector<std::vector<float> > outputs  {{0}, {1}, {1}, {0}};
-
-  int bestNetworkIndex = 0;
-  float bestNetworkScore = 999;
-  
-  for (int i = 0; i < 10000 && bestNetworkScore != 0; i++) {
-    for (int k = 0; k < networks.size(); k++) {
-      float score = 0;
-      for (int j = 0; j < inputs.size(); j++) {
-  	networks[k]->setInputs(inputs[j]);
-  	networks[k]->compute();
-  	score +=  fabs(networks[k]->getOutputs()[0]->getOutput() - outputs[j][0]);
-      }
-      if(score < bestNetworkScore) {
-  	bestNetworkScore = score;
-  	bestNetworkIndex = k;
-      }
-    }
-    for (int i = 0; i < networks.size(); i++) {
-      if(i != bestNetworkIndex) {
-  	networks[i]->copyNetworkValues(networks[bestNetworkIndex]);
-  	networks[i]->mutate(0.05f);
-      }
-    }
-      
+  initscr();
+  cbreak();
+  printw("hello world");
+  game->connectNetworks(networks);
+  for(;;) {
+    refresh();
+    game->update();
+    refresh();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    clear();
   }
 
+  /*******************/
+  // std::vector<std::vector<float> > inputs = {{0, 0, 1}, {1, 1, 1}, {1, 0, 1}, {0, 1, 1}};
+  // std::vector<std::vector<float> > outputs  {{0}, {1}, {1}, {0}};
+  // int bestNetworkIndex = 0;
+  // float bestNetworkScore = 9999999999;
   
-  // Network n(dimensions);
-  // n.setInputs(inputs[0]);
-  // n.compute();
-  // std::cout << "output = " << n.getOutputs()[0]->getOutput() << "\n";
-
-  // for (int i = 0;; i++) {
-  //   n.mutate(0.1f);
-  //   n.compute();
-  //   std::cout << "output = " << n.getOutputs()[0]->getOutput() << "          " << "\r";
+  // for (int i = 0; i < 1000 && bestNetworkScore != 0; i++) {
+  //   // std::cout << "iteration : " << i << " --- " << "best score = " << bestNetworkScore << "\n";
+  //   for (int k = 0; k < networks.size(); k++) {
+  //     float score = 0;
+  //     for (int j = 0; j < inputs.size(); j++) {
+  // 	networks[k]->setInputs(inputs[j]);
+  // 	networks[k]->compute();
+  // 	score +=  fabs(networks[k]->getOutputs()[0]->getOutput() - outputs[j][0]);
+  //     }
+  //     if(score < bestNetworkScore) {
+  // 	bestNetworkScore = score;
+  // 	bestNetworkIndex = k;
+  //     }
+  //   }
+  //   for (int a = 0; a < networks.size(); a++) {
+  //     if(i != bestNetworkIndex) {
+  // 	networks[a]->copyNetworkValues(networks[bestNetworkIndex]);
+  // 	networks[a]->mutate(0.05f);
+  //     }
+  //   }
   // }
-
+  /*******************/
+  
+  
   // std::vector<std::vector<float> > inputs = {{0, 0, 1}, {1, 1, 1}, {1, 0, 1}, {0, 1, 1}};
   // std::vector<std::vector<float> > outputs  {{0}, {1}, {1}, {0}};
 
@@ -71,16 +76,8 @@ int main(int argc, char **argv) {
   // std::vector<std::vector<float> > inputs = {{0, 0, 0}, {1, 1, 1}, {0, 1, 1}, {0, 1, 0}};
   // std::vector<std::vector<float> > outputs  {{0}, {1}, {1}, {0}};
 
-
-  // for (int i = 0; i < 100000; i++) {
-  //   for (int j = 0; j < inputs.size(); j++) {
-  //     n.train(inputs[j], outputs[j]);
-  //   }
-  //   // n.getOutputs()[0]->getOutput();
-  // }
-
-  networks[bestNetworkIndex]->setInputs({1, 1, 0});
-  networks[bestNetworkIndex]->compute();
-  std::cout << "result = " << networks[bestNetworkIndex]->getOutputs()[0]->getOutput() << "\n";
-  
+  // networks[bestNetworkIndex]->setInputs({1, 0, 0});
+  // networks[bestNetworkIndex]->compute();
+  // std::cout << "result = " << networks[bestNetworkIndex]->getOutputs()[0]->getOutput() << "\n";
+  endwin();
 }
